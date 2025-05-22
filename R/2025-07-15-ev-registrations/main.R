1149ecbe5280be1786591fe845ad1322da913539
asenetcky <me@asenetcky.dev>
2025-05-22 15:12:04 +0000

    feat(script): add data pull and save

---
+++ R/2025-07-15-ev-registrations/main.R
@@ -0,0 +1,64 @@
+# EV Registrations
+
+## Env
+### Using `renv` but trying to make as `renv` agnostic as possible
+library(dplyr) # for data wrangling
+library(glue) # for string literals
+library(httr2) # for http requests
+library(ggplot2) # for data visualizations
+library(nanoparquet) # low dependancy parquet files
+library(fs) # file manipulations
+library(lubridate) # date manipulations
+
+## Data Pull
+domain <- "https://data.ct.gov/"
+resource <- "resource/y7ky-5wcz.json"
+
+### too lazy to loop through - just going to grab > 70k (should be enough)
+### API uses LIMITS and OFFSETS
+limit <- 70000L
+limit_string <- glue::glue("$limit={limit}")
+
+### combine it all
+endpoint <- glue::glue(
+    "{domain}{resource}?{limit_string}"
+)
+
+### GET request
+req <- request(endpoint)
+req |> req_dry_run()
+
+### GET response
+resp <- req_perform(req)
+
+### convert resp to data we can use
+data <- 
+    resp |>
+    resp_body_json() |>
+    bind_rows() |>
+    mutate(
+        across(
+            .cols = c(
+                id, 
+                vehicleweight, 
+                vehicleyear,
+                vehicledeclaredgrossweight,
+                vehiclerecordedgvwr,
+                ),
+            .fns = as.numeric
+        ),
+        across(
+            .cols = c(registration_date_start, registration_date_expiration),
+            .fns = as_date
+        )
+    )
+
+### take a look
+glimpse(data)
+
+### parquet this for later
+write_parquet(data, path_wd("data", "ev-registrations", ext = "parquet"))
+
+## Data Wrangle
+
+## Visualization
